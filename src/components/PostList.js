e009d5c6f8b4256b34189820a4ba9dcc4be67b06
Nisfuuu <nisfu4553@gmial.com>
2024-11-19 09:45:10 +0700

    menambahkan styling pada card postlist

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,10 @@
+// components/PostList.js
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Loader from "./Loader";
-import "./PostList.css";
+import "../App.css"; // Pastikan Anda mengimpor CSS
 
-const App = () => {
+const PostList = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true); // State untuk mengontrol loading
 
@@ -25,86 +26,30 @@ const App = () => {
   if (loading) return <Loader />; // Tampilkan Loader jika loading
 
   return (
-    <div>
-      <h1>Daftar Post</h1>
-
-      <div className="card">
-        <svg
-          className="img"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlSpace="preserve"
-          width="100%"
-          height="100%"
-          version="1.1"
-          shapeRendering="geometricPrecision"
-          textRendering="geometricPrecision"
-          imageRendering="optimizeQuality"
-          fillRule="evenodd"
-          clipRule="evenodd"
-          viewBox="0 0 784.37 1277.39"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-        >
-          <g id="Layer_x0020_1">
-            <metadata id="CorelCorpID_0Corel-Layer" />
-            <g id="_1421394342400">
-              <g>
-                <polygon
-                  fill="#343434"
-                  fillRule="nonzero"
-                  points="392.07,0 383.5,29.11 383.5,873.74 392.07,882.29 784.13,650.54"
-                />
-                <polygon
-                  fill="#8C8C8C"
-                  fillRule="nonzero"
-                  points="392.07,0 -0,650.54 392.07,882.29 392.07,472.33"
-                />
-                <polygon
-                  fill="#3C3C3B"
-                  fillRule="nonzero"
-                  points="392.07,956.52 387.24,962.41 387.24,1263.28 392.07,1277.38 784.37,724.89"
-                />
-                <polygon
-                  fill="#8C8C8C"
-                  fillRule="nonzero"
-                  points="392.07,1277.38 392.07,956.52 -0,724.89"
-                />
-                <polygon
-                  fill="#141414"
-                  fillRule="nonzero"
-                  points="392.07,882.29 784.13,650.54 392.07,472.33"
-                />
-                <polygon
-                  fill="#393939"
-                  fillRule="nonzero"
-                  points="0,650.54 392.07,882.29 392.07,472.33"
-                />
-              </g>
-            </g>
-          </g>
-        </svg>
-        <div className="textBox">
-          <p className="text head">Ethereum</p>
-          <span>Cryptocurrency</span>
-          <p className="text price">1.654,34€</p>
-        </div>
-      </div>
-
+    <div className="container">
+      {" "}
+      {/* Menggunakan kelas container untuk grid */}
+      <h1 style={{ width: "100%", textAlign: "center" }}>Daftar Post</h1>
       {posts.map((post) => (
         <Link
           to={`/posts/${post.id}`}
           key={post.id}
-          style={{ marginBottom: "20px", display: "block" }} // Menambahkan display block agar link lebih mudah diklik
+          style={{ textDecoration: "none" }} // Menghilangkan garis bawah pada link
         >
-          <h2>{post.title}</h2>
-          <img
-            src={`https://picsum.photos/300/200?random=${post.id}`} // Menggunakan ID post untuk mendapatkan gambar acak
-            alt={`Gambar untuk ${post.title}`}
-            style={{ width: "300px", height: "200px" }}
-          />
+          <div className="card">
+            {" "}
+            {/* Menambahkan kelas card */}
+            <h2 className="card-title">{post.title}</h2>{" "}
+            {/* Menambahkan kelas card-title */}
+            <img
+              src={`https://picsum.photos/300/200?random=${post.id}`} // Menggunakan ID post untuk mendapatkan gambar acak
+              alt={`Gambar untuk ${post.title}`}
+            />
+          </div>
         </Link>
       ))}
     </div>
   );
 };
 
-export default App;
+export default PostList;
